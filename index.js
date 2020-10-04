af59077da89b0cedb81dc87c9d59b4e0c808d649
Sahi Joshi <sahik.joshi@gmail.com>
2020-10-04 14:53:06 +0300

    Refactor code to parse txt data and move the logic to scheduler.

--- index.js
+++ index.js
@@ -1,50 +1,23 @@
 var express = require("express");
 var indexRouter = require('./src/routes/index');
 var config = require('./src/config/config');
-// var scheduler = require('./src/scheduler/scheduler');
-// var x = require('./src/scheduler/scheduler');
-var fs = require('fs');
-var registerTable = require('./src/util/registerTable')
-const FILE_NAME = 'feed.txt';
+var scheduler = require('./src/scheduler/scheduler');
 
 const app = express();
 
 app.use('/', indexRouter);
 
+// scheduler.x();
+
 /**
  * Run cron job to fetch data from Modbus live feed.
  */
 
+scheduler.start();
 
-// scheduler.start();
-// console.log(scheduler.x)
 /**
  * Create HTTP and listen on port.
  */
-
-// let a = ["1:3", "1:2","2:3","3:4"];
-// let a = '[{"date": "2018-12-12"}]';
-
-// const b = JSON.parse(a);
-
-// console.log(b);
-
-// const array = fs
-// .readFileSync(FILE_NAME, 'utf-8')
-// .toString()
-// .replace(/\r\n/g,'\n')
-// .split('\n');
-
-// let date = array[0];
-// console.log(date);
-// console.log(array);
-
-// for(let i of array) {
-//     console.log(i);
-// }
-
-console.log(registerTable["1-2"]["varName"]);
-
 
 app.listen(config.port, () => {
     console.log('success');
--- src/scheduler/scheduler.js
+++ src/scheduler/scheduler.js
@@ -1,28 +1,69 @@
 var cron = require('node-cron');
 var fs = require('fs');
+var registerTable = require('../util/registerTable');
+var config = require('../config/config');
+
 const FILE_NAME = 'feed.txt';
 
-const x = mapEvents(pullModubusLiveFeedData(FILE_NAME));
+// Load data in txt format and convert into array of string.
 
-function pullModubusLiveFeedData(filename) {
-    return fs
-    .readFileSync(filename, 'utf-8')
-    .toString()
-    .replace(/\r\n/g,'\n')
-    .split('\n');
-    // .split('\n')
-    // .filter(Boolean)
-    // .map(JSON.parse);
+function loadModubusLiveFeedData() {
+    const rawDataArr = fs
+        .readFileSync(FILE_NAME, 'utf-8')
+        .toString()
+        .replace(/\r\n/g, '\n')
+        .split('\n');
+    
+    convertRawDataIntoJson(rawDataArr);
+}
+
+// Parse Raw txt data and covert into JSON format.
+
+function convertRawDataIntoJson(rawDataArr) {
+    var cookedRegisterArr = [];
+
+    let processedDataArr = rawDataArr.filter((x,i) => {return (i > 0) && x.length > 0}).map((value, index) => value.split(':'));
+    
+    var i = 0;
+    do {
+        let registerNumber = processedDataArr[i][0];
+        if (typeof registerTable[registerNumber] !== 'undefined') {
+            if (registerTable[registerNumber]["number"] == 1) {
+                cookedRegisterArr.push({"register": `${processedDataArr[i][0]}`, "value": `${processedDataArr[i][1]}`, "unit": registerTable[registerNumber]["unit"], "name": registerTable[registerNumber]["varName"]});
+                i += 1;
+            } else if (registerTable[registerNumber]["number"] == 2) {
+                cookedRegisterArr.push({"register": `${processedDataArr[i][0]}-${processedDataArr[i+1][0]}`, "value": `${processedDataArr[i][1]}-${processedDataArr[i+1][1]}`, "unit": registerTable[registerNumber]["unit"], "name": registerTable[registerNumber]["varName"]});
+                i += 2;
+            } else if (registerTable[registerNumber]["number"] == 3) {
+                cookedRegisterArr.push({"register": `${processedDataArr[i][0]}-${processedDataArr[i+1][0]}-${processedDataArr[i+2][0]}`, "value": `${processedDataArr[i][1]}-${processedDataArr[i+1][1]}-${processedDataArr[i+2][1]}`, "unit": registerTable[registerNumber]["unit"], "name": registerTable[registerNumber]["varName"]});
+                i += 3;
+            }
+        } else {
+            i += 1;
+        }
+    }
+    while (i < processedDataArr.length);
+    
+    var finalRegisterDataArr = {"data": cookedRegisterArr, "date": processedDataArr[0]};
+    var registerJsonData = JSON.stringify(finalRegisterDataArr);
+    console.log(JSON.stringify(registerJsonData));
+    saveJsonFile(registerJsonData);
+}
+
+function saveJsonFile(registerJsonData) {
+    fs.writeFile(__dirname + config.jsonFileName, registerJsonData, 'utf-8', function (err) {
+        if (err) {
+            console.log("failed to save")
+        } else {
+            console.log("succeeded in saving")
+        }
+    });
 }
 
 let task = cron.schedule('* */1 * * * *', () => {
-    // pullModubusLiveFeedData();
+    loadModubusLiveFeedData();
     console.log('running a task every two minutes');
 });
-
-function mapEvents(events) {
-    return JSON.stringify(events);
-}
   
 module.exports = task;
-module.exports = x;
+// module.exports = x;
