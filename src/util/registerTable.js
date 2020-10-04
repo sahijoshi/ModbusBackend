66ce74d6f167be6d74eab61b12df7accfacf43a3
Sahi Joshi <sahik.joshi@gmail.com>
2020-10-04 13:24:44 +0300

    Update register table with number of registers required

--- src/util/registerTable.js
+++ src/util/registerTable.js
@@ -1,276 +1,321 @@
-
 const registerTable = {
-    "1-2": {
+    "1": {
+        "number": 2,
         "varName": "Flow Rate",
         "format": "REAL4",
         "unit": "m3/h",
         "realValue": ""
     },
-    "3-4": {
+    "3": {
+        "number": 2,
         "varName": "Energy Flow Rate",
         "format": "REAL4",
         "unit": "GJ/h",
         "realValue": ""
     },
-    "5-6": {
+    "5": {
+        "number": 2,
         "varName": "Velocity",
         "format": "REAL4",
         "unit": "m/s",
         "realValue": ""
     },
-    "7-8": {
+    "7": {
+        "number": 2,
         "varName": "Fluid sound speed",
         "format": "REAL4",
         "unit": "m/s",
         "realValue": ""
     },
-    "9-10": {
+    "9": {
+        "number": 2,
         "varName": "Positive accumulator",
         "format": "LONG",
         "unit": "",
         "realValue": ""
     },
-    "11-12": {
+    "11": {
+        "number": 2,
         "varName": "Positive decimal fraction",
         "format": "REAL4",
         "unit": "",
         "realValue": ""
     },
-    "13-14": {
+    "13": {
+        "number": 2,
         "varName": "Negative accumulator",
         "format": "LONG",
         "unit": "",
         "realValue": ""
     },
-    "15-16": {
+    "15": {
+        "number": 2,
         "varName": "Negative decimal fraction",
         "format": "REAL4",
         "unit": "",
         "realValue": ""
     },
-    "17-18": {
+    "17": {
+        "number": 2,
         "varName": "Positive energy accumulator",
         "format": "LONG",
         "unit": "",
         "realValue": ""
     },
-    "19-20": {
+    "19": {
+        "number": 2,
         "varName": "Positive energy decimal fraction",
         "format": "REAL4",
         "unit": "",
         "realValue": ""
     },
-    "21-22": {
+    "21": {
+        "number": 2,
         "varName": "Negative energy accumulator",
         "format": "LONG",
         "unit": "",
         "realValue": ""
     },
-    "23-24": {
+    "23": {
+        "number": 2,
         "varName": "Negative energy decimal fraction",
         "format": "REAL4",
         "unit": "",
         "realValue": ""
     },
-    "25-26": {
+    "25": {
+        "number": 2,
         "varName": "Net accumulator",
         "format": "LONG",
         "unit": "",
         "realValue": ""
     },
-    "27-28": {
+    "27": {
+        "number": 2,
         "varName": "Net decimal fraction",
         "format": "REAL4",
         "unit": "",
         "realValue": ""
     },
-    "29-30": {
+    "29": {
+        "number": 2,
         "varName": "Net energy accumulator",
         "format": "LONG",
         "unit": "",
         "realValue": ""
     },
-    "31-32": {
+    "31": {
+        "number": 2,
         "varName": "Net energy decimal fraction",
         "format": "REAL4",
         "unit": "",
         "realValue": ""
     },
-    "33-34": {
+    "33": {
+        "number": 2,
         "varName": "Temperature #1/inlet",
         "format": "REAL4",
         "unit": "C",
         "realValue": ""
     },
-    "35-36": {
+    "35": {
+        "number": 2,
         "varName": "Temperature #2/outlet",
         "format": "REAL4",
         "unit": "C",
         "realValue": ""
     },
-    "37-38": {
+    "37": {
+        "number": 2,
         "varName": "Analog input AI3",
         "format": "REAL4",
         "unit": "",
         "realValue": ""
     },
-    "39-40": {
+    "39": {
+        "number": 2,
         "varName": "Analog input AI4",
         "format": "REAL4",
         "unit": "",
         "realValue": ""
     },
-    "41-42": {
+    "41": {
+        "number": 2,
         "varName": "Analog input AI5",
         "format": "REAL4",
         "unit": "",
         "realValue": ""
     },
-    "43-44": {
+    "43": {
+        "number": 2,
         "varName": "Current input at AI3",
         "format": "REAL4",
         "unit": "mA",
         "realValue": ""
     },
-    "45-46": {
+    "45": {
+        "number": 2,
         "varName": "Current input at AI3",
         "format": "REAL4",
         "unit": "mA",
         "realValue": ""
     },
-    "47-48": {
+    "47": {
+        "number": 2,
         "varName": "Current input at AI3",
         "format": "REAL4",
         "unit": "",
         "realValue": ""
     },
-    "49-50": {
+    "49": {
+        "number": 2,
         "varName": "System password",
         "format": "BCD",
         "unit": "",
         "realValue": ""
     },
     "51": {
+        "number": 1,
         "varName": "Password for hardware",
         "format": "BCD",
         "unit": "",
         "realValue": ""
     },
-    "53-55": {
+    "53": {
+        "number": 3,
         "varName": "Calendar (date and time)",
         "format": "BCD",
         "unit": "",
         "realValue": ""
     },
     "56": {
+        "number": 1,
         "varName": "Day+Hour for Auto-Save",
         "format": "BCD",
         "unit": "",
         "realValue": ""
     },
     "59": {
+        "number": 1,
         "varName": "Key to input",
         "format": "INTEGER",
         "unit": "",
         "realValue": ""
     },
     "60": {
+        "number": 1,
         "varName": "Go to Window #",
         "format": "INTEGER",
         "unit": "",
         "realValue": ""
     },
     "61": {
+        "number": 1,
         "varName": "LCD Back-lit lights for number of seconds",
         "format": "INTEGER",
         "unit": "",
         "realValue": ""
     },
     "62": {
+        "number": 1,
         "varName": "Times for the beeper",
         "format": "INTEGER",
         "unit": "",
         "realValue": ""
     },
     "72": {
+        "number": 1,
         "varName": "Error Code",
         "format": "BIT",
         "unit": "",
         "realValue": ""
     },
-    "77-78": {
+    "77": {
+        "number": 2,
         "varName": "PT100 resistance of inlet",
         "format": "REAL4",
         "unit": "Ohm",
         "realValue": ""
     },
-    "79-80": {
+    "79": {
+        "number": 2,
         "varName": "PT100 resistance of outlet",
         "format": "REAL4",
         "unit": "Ohm",
         "realValue": ""
     },
-    "81-82": {
+    "81": {
+        "number": 2,
         "varName": "Total travel time",
         "format": "REAL4",
         "unit": "Micro-second",
         "realValue": ""
     },
-    "83-84": {
+    "83": {
+        "number": 2,
         "varName": "Delta travel time",
         "format": "REAL4",
         "unit": "Nino-second",
         "realValue": ""
     },
-    "85-86": {
+    "85": {
+        "number": 2,
         "varName": "Upstream travel time",
         "format": "REAL4",
         "unit": "Micro-second",
         "realValue": ""
     },
-    "87-88": {
+    "87": {
+        "number": 2,
         "varName": "Downstream travel time",
         "format": "REAL4",
         "unit": "Micro-second",
         "realValue": ""
     },
-    "89-90": {
+    "89": {
+        "number": 2,
         "varName": "Output current",
         "format": "REAL4",
         "unit": "mA",
         "realValue": ""
     },
     "92": {
+        "number": 1,
         "varName": "Signal Quality",
         "format": "INTEGER",
         "unit": "",
         "realValue": ""
     },
     "93": {
+        "number": 1,
         "varName": "Upstream strength",
         "format": "INTEGER",
         "unit": "",
         "realValue": ""
     },
     "94": {
+        "number": 1,
         "varName": "Downstream strength",
         "format": "INTEGER",
         "unit": "",
         "realValue": ""
     },
     "96": {
+        "number": 1,
         "varName": "Language used in user interface",
         "format": "INTEGER",
         "unit": "",
         "realValue": ""
     },
-    "97-98": {
+    "97": {
+        "number": 2,
         "varName": "The rate of the measured travel time by the calculated travel time",
         "format": "REAL4",
         "unit": "",
         "realValue": ""
     },
-    "99-100": {
+    "99": {
+        "number": 2,
         "varName": "Reynolds number",
         "format": "REAL4",
         "unit": "",
