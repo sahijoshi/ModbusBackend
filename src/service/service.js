a3d2011f881931a32f6078811045dd0b79b0bae2
Sahi Joshi <sahik.joshi@gmail.com>
2020-10-07 04:51:07 +0300

    Refactor txt parsing logic

--- src/service/service.js
+++ src/service/service.js
@@ -19,7 +19,6 @@ const loadModbusData = function() {
             const rawDataArr = str.split('\n');
             convertRawDataIntoJson(rawDataArr);
         });
-
   });
 }
 
@@ -29,54 +28,77 @@ function convertRawDataIntoJson(rawDataArr) {
     var cookedRegisterArr = [];
     let processedDataArr = rawDataArr.filter((x,i) => {return (i > 0) && x.length > 0}).map((value, index) => value.split(':'));
     
-    var redableRegisterValue = "";
-
-    var i = 0;
+    var index = 0;
     do {
-        redableRegisterValue = "";
-        let registerNumber = processedDataArr[i][0];
-        if (typeof registerTable[registerNumber] !== 'undefined') {
-            if (registerTable[registerNumber]["number"] == 1) {
-                let registerNumber = processedDataArr[i][0];
-                // Handled conversion logic only for signal quality.
-                // TODO: Handle for other registers and data types.
-                if (registerNumber == 92) {
-                    let registerValue = processedDataArr[i][1];
-                    redableRegisterValue = getRealValueForSignalQuality(registerValue);
-                }
-                cookedRegisterArr.push({"register": `${processedDataArr[i][0]}`, "regiter_value": `${processedDataArr[i][1]}`, "unit": registerTable[registerNumber]["unit"], "variable_name": registerTable[registerNumber]["varName"], "real_value": redableRegisterValue});
-                i += 1;
-            } else if (registerTable[registerNumber]["number"] == 2) {
-                let format = registerTable[registerNumber]["format"];
-                // Handled conversion login only for LONG format.
-                // TODO: Handle for all other data format.
-                if (format == "LONG") {
-                    let registerValue1 = processedDataArr[i][1];
-                    let registerValue2 = processedDataArr[i+1][1];
-                    redableRegisterValue = getRealValueForTwoRegister(registerValue1, registerValue2);
-                }
-                cookedRegisterArr.push({"register": `${processedDataArr[i][0]}-${processedDataArr[i+1][0]}`, "regiter_value": `${processedDataArr[i][1]}-${processedDataArr[i+1][1]}`, "unit": registerTable[registerNumber]["unit"], "variable_name": registerTable[registerNumber]["varName"], "real_value": redableRegisterValue});
-                i += 2;
-            } else if (registerTable[registerNumber]["number"] == 3) {
-                cookedRegisterArr.push({"register": `${processedDataArr[i][0]}-${processedDataArr[i+1][0]}-${processedDataArr[i+2][0]}`, "regiter_value": `${processedDataArr[i][1]}-${processedDataArr[i+1][1]}-${processedDataArr[i+2][1]}`, "variable_name": registerTable[registerNumber]["unit"], "name": registerTable[registerNumber]["varName"], "real_value": redableRegisterValue});
-                i += 3;
+        let registerId = processedDataArr[index][0];
+        if (typeof registerTable[registerId] !== 'undefined') { 
+            let registerNumber = registerTable[registerId]["number"];
+            if (registerNumber == 1) {
+                cookedRegisterArr.push(getNewRegister(processedDataArr,registerId, registerNumber, index));
+                index += 1;
+            } else if (registerNumber == 2) {
+                cookedRegisterArr.push(getNewRegister(processedDataArr, registerId, registerNumber, index));
+                index += 2;
+            } else if (registerNumber == 3) {
+                cookedRegisterArr.push(getNewRegister(processedDataArr, registerId, registerNumber, index));
+                index += 3;
             }
         } else {
-            i += 1;
+            index += 1;
         }
     }
-    while (i < processedDataArr.length);
+    while (index < processedDataArr.length);
     
     var finalRegisterDataArr = 
         {
         "date": rawDataArr[0], 
-        "header_key": ["register", "variable_name", "unit", "regiter_value", "real_value"],
-        "header_value": {"register": "Register", "variable_name": "Varaible Name", "unit": "Unit", "regiter_value": "Register Value", "real_value": "Real Value"},
+        "header_key": ["register", "variable_name", "unit", "register_value", "real_value"],
+        "header_value": {"register": "Register", "variable_name": "Varaible Name", "unit": "Unit", "register_value": "Register Value", "real_value": "Real Value"},
         "data": cookedRegisterArr 
         };
     var registerJsonData = JSON.stringify(finalRegisterDataArr);
     console.log(registerJsonData);
     saveJsonFile(registerJsonData);
+}
+
+function getNewRegister(processedDataArr, registerId, registerNumber, index) {
+    var redableRegisterValue = "";
+    var register = "";
+    var registerValue = "";
+    var dash = "";
+    var tempIndex = index;
+    for (var i = 0; i < registerNumber; i++) {
+        register = register + dash + `${processedDataArr[tempIndex][0]}`;
+        registerValue = registerValue + dash + `${processedDataArr[tempIndex][1]}`;
+        dash = "-";
+        tempIndex++;
+    }
+
+    if (registerNumber == 1) {
+        let registerNumber = processedDataArr[index][0];
+        // Handled conversion logic only for signal quality.
+        // TODO: Handle for other registers and data types.
+        if (registerNumber == 92) {
+            let registerValue = processedDataArr[index][1];
+            redableRegisterValue = getRealValueForSignalQuality(registerValue);
+        }
+    } else if (registerNumber == 2) {
+        let format = registerTable[registerId]["format"];
+        // Handled conversion login only for LONG format.
+        // TODO: Handle for all other data format.
+        if (format == "LONG") {
+            let registerValue1 = processedDataArr[index][1];
+            let registerValue2 = processedDataArr[index+1][1];
+            redableRegisterValue = getRealValueForTwoRegister(registerValue1, registerValue2);
+        }
+    } 
+
+    return {
+        "register": register, 
+        "register_value": registerValue, 
+        "unit": registerTable[registerId]["unit"], 
+        "variable_name": registerTable[registerId]["varName"], 
+        "real_value": redableRegisterValue};
 }
 
 // Save json file to persistence folder used for response to the client
